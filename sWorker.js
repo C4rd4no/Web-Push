294adf5ac4089f3c690b0989dc1d2704c66ed0bf
Horváth Tamás <horvath.tamas.gy@dolphio.hu>
2021-06-16 15:02:41 +0200

    b

--- sWorker.js
+++ sWorker.js
@@ -1,3 +1,25 @@
 self.addEventListener('push', (e) => {
-  e.waitUntil(self.registration.showNotification('Random Cat for you!', {}));
+  var options = {
+    body: 'Let see',
+    icon: '',
+    vibrate: [100,50,100],
+    data:{
+      dateOfArrival:Date.now(),
+      primaryKey: '2',
+    },
+    actions:[
+      {
+        action: 'explore',
+        title:'Explore this new world',
+        icon: ''
+      },
+      {
+        action: 'close',
+        title:'Close',
+        icon: ''
+      }
+    ]
+
+  }
+  e.waitUntil(self.registration.showNotification('Random Cat for you!', options));
 });
