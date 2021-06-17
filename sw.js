e6bc4f71ab7976d3a4c299e41e13bf21da4f0c4a
Horváth Tamás <horvath.tamas.gy@dolphio.hu>
2021-06-17 08:09:50 +0200

    a

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
 self.addEventListener('push', () => {
+  var options = {
+    body: 'Let see',
+    icon: '',
+    vibrate: [100,50,100],
+    data:{
+      dateOfArrival:Date.now(),
+      primaryKey: '2',
+    },
+    actions:[
+      {
+        action: 'explore',
+        title:'Explore this new world',
+        icon: ''
+      },
+      {
+        action: 'close',
+        title:'Close',
+        icon: ''
+      }
+    ]
+  }  
+
   self.registration.showNotification('Hello world!', options);
 });
