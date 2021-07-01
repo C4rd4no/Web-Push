90f437a90d42e22786c87512cd0c685e023377d1
Horváth Tamás <horvath.tamas.gy@dolphio.hu>
2021-07-01 12:26:57 +0200

    b

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 self.addEventListener('push', (e) => {
-  console.log(e.data.json());
+  const payload = e.data.json();
   var options = {
     
-    body: JSON.parse(e.payload).body,
-    icon: JSON.parse(e.payload).iconUrl,
-    image: JSON.parse(e.payload).imageUrl,
+    body: payload.body,
+    icon: payload.iconUrl,
+    image: payload.imageUrl,
     data:{
       dateOfArrival:Date.now(),
-      url: JSON.parse(e.payload).onclickUrl,
+      url: payload.onclickUrl,
     },
   };  
 
-  e.waitUntil(self.registration.showNotification(JSON.parse(e.payload).title, options));
+  e.waitUntil(self.registration.showNotification(payload.title, options));
 });
