6dbf2d24f66a3f6b0cf7d89c793c6a8b3df0765b
Horváth Tamás <horvath.tamas.gy@dolphio.hu>
2021-07-05 14:44:33 +0200

    dsf

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 self.addEventListener('push', (e) => {
+  if(current.time)
   const payload = e.data.json();
+
+  var time = {
+    hour: today.getHours(),
+    min:today.getMinutes()
+  };
 
   const options = {
     body: payload.body,
@@ -8,9 +14,11 @@ self.addEventListener('push', (e) => {
     data:{
       url: payload.onclickUrl,
     },
-  };  
+  };
 
-  e.waitUntil(self.registration.showNotification(payload.title, options));
+  if(payload.time.min === time.min){
+    e.waitUntil(self.registration.showNotification(payload.title, options));
+  }
 });
 
 self.addEventListener("notificationclick", (e) => {
