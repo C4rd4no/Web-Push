ccd58766ad0d6432e056ede526b1ee1c44728c22
Horváth Tamás <horvath.tamas.gy@dolphio.hu>
2021-07-09 13:05:27 +0200

    asdasd

--- sw.js
+++ sw.js
@@ -1,10 +1,32 @@
-const send = (payload) => {
-  var date = new Date();
-  var time = {
-    hour: date.getHours(),
-    min: date.getMinutes()
-  };
-  var options = {
+// const send = (payload) => {
+//   var date = new Date();
+//   var time = {
+//     hour: date.getHours(),
+//     min: date.getMinutes()
+//   };
+//   var options = {
+//     body: payload.body,
+//     icon: payload.iconUrl,
+//     image: payload.imageUrl,
+//     data:{
+//       url: payload.onclickUrl,
+//     },
+//   };
+//   if(payload.time.min === time.min){
+//     console.log("ok");
+//     self.registration.showNotification(payload.title, options);
+//   }else{
+//     setTimeout(() => self.registration.getNotifications().then(function(notifications) {
+//       notifications.forEach(notification => notification.close());
+//     }), 500)
+    
+//   }
+// }
+
+self.addEventListener('push', (e) => {
+  var payload = e.data.json();
+  
+    var options = {
     body: payload.body,
     icon: payload.iconUrl,
     image: payload.imageUrl,
@@ -12,20 +34,8 @@ const send = (payload) => {
       url: payload.onclickUrl,
     },
   };
-  if(payload.time.min === time.min){
-    console.log("ok");
-    self.registration.showNotification(payload.title, options);
-  }else{
-    setTimeout(() => self.registration.getNotifications().then(function(notifications) {
-      notifications.forEach(notification => notification.close());
-    }), 500)
-    
-  }
-}
 
-self.addEventListener('push', (e) => {
-  var payload = e.data.json();
-  e.waitUntil(send(payload));
+  e.waitUntil(self.registration.showNotification(payload.title, options));
 });
 
 self.addEventListener("notificationclick", (e) => {
